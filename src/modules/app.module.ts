45cf2f1f372b528e6952a10911e3edb508a30307
Andy Tsai <bbandydd@gmail.com>
2018-01-04 16:21:27 +0800

    remove chat module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,14 +2,13 @@ import { Module, RequestMethod, UseFilters } from '@nestjs/common';
 import { UsersController } from './Users/users.controller';
 import { ProductsController } from './Products/products.controller';
 import { UsersModule } from './Users/users.module';
-import { ChatModule } from './Chat/chat.module';
 import { LoggerMiddleware } from './Shared/middlewares/logger.middleware';
 import { SimpleMiddleware } from './Shared/middlewares/simple.middleware';
 import { HttpExceptionFilter } from './Shared/ExceptionFilters/http-exception.filter';
 import { NestModule, MiddlewaresConsumer } from '@nestjs/common/interfaces';
 
 @Module({
-  modules: [UsersModule, ChatModule],
+  modules: [UsersModule],
 })
 // @UseFilters(new HttpExceptionFilter())
 export class ApplicationModule implements NestModule {
